1cc24e855443f492a06e36865c5539a3b6c7e0c5
tim <2625509651@qq.com>
2021-01-06 21:52:29 +0800

    third

--- js/cart.js
+++ js/cart.js
@@ -5,3 +5,12 @@ $('.res').mouseout(function(){
     $(this).children('.cmg').css('display','none')
 })
 
+$('.flex2').mouseover(function(){
+    $(this).children('.flex2>p:nth-of-type(3)').css('display','none')
+    $(this).children('.flex2 button').css('display','block')
+})
+
+$('.flex2').mouseout(function(){
+    $(this).children('.flex2>p:nth-of-type(3)').css('display','block')
+    $(this).children('.flex2 button').css('display','none')
+})
---
+++ js/detail.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.res').mouseover(function(){
+    $(this).children('.cmg').css('display','block')
+})
+$('.res').mouseout(function(){
+    $(this).children('.cmg').css('display','none')
+})
